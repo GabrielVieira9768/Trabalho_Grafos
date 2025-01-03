b6beb72ee2da4beaa67dd69a88eee6d1c72f94bc
Maria Luiza Dornelas <maria.dornelas@engenharia.ufjf.br>
2025-01-02 21:37:58 -0300

    Alterações iniciais

--- grafo_matriz.h
+++ grafo_matriz.h
@@ -2,12 +2,13 @@
 #define GRAFOMATRIZ_H
 
 #include "Grafo.h"
-#include <vector>
 
 class GrafoMatriz : public Grafo {
     private:
         int numero_vertices;
-        std::vector<std::vector<int>> matriz;
+
+    public:
+        bool eh_bipartido() override;
 };
 
 #endif // GRAFOMATRIZ_H
