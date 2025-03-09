a5d96a89c61fc035f5d37b5f1954952ec31faedd
mariadornelas <mariadornelasc12@gmail.com>
2025-03-09 19:25:27 -0300

    Código do guloso que ta dando certo

--- src/grafo.cpp
+++ src/grafo.cpp
@@ -11,96 +11,120 @@
 
 using namespace std;
 
-int Grafo::get_ordem() {
+int Grafo::get_ordem()
+{
     return this->ordem;
 }
 
-bool Grafo::eh_direcionado() {
+bool Grafo::eh_direcionado()
+{
     return this->direcionado;
 }
 
-bool Grafo::vertice_ponderado() {
+bool Grafo::vertice_ponderado()
+{
     return this->verticePonderado;
 }
 
-bool Grafo::aresta_ponderada() {
+bool Grafo::aresta_ponderada()
+{
     return this->arestaPonderada;
 }
 
-void Grafo::novo_no(int id, float peso) {
+void Grafo::novo_no(int id, float peso)
+{
     cerr << "Método novo_no chamado na classe base" << endl;
 }
 
-void Grafo::deleta_no(int id) {
+void Grafo::deleta_no(int id)
+{
     cerr << "Método deleta_no chamado na classe base" << endl;
 }
 
-void Grafo::nova_aresta(int origem, int destino, float peso) {
-    cerr << "Método nova_aresta chamado na classe base"<< endl;
+void Grafo::nova_aresta(int origem, int destino, float peso)
+{
+    cerr << "Método nova_aresta chamado na classe base" << endl;
 }
 
-void Grafo::deleta_aresta(int origem, int destino) {
+void Grafo::deleta_aresta(int origem, int destino)
+{
     cerr << "Método deleta_aresta chamado na classe base" << endl;
 }
 
-float Grafo::getPesoAresta(int origem, int destino) {
+float Grafo::getPesoAresta(int origem, int destino)
+{
     return 0;
 }
 
-bool Grafo::existeNo(int id) {
-  return false;
+bool Grafo::existeNo(int id)
+{
+    return false;
 }
 
-bool Grafo::existeAresta(int origem, int destino) {
-  return false;
+bool Grafo::existeAresta(int origem, int destino)
+{
+    return false;
 }
 
-int* Grafo::getVizinhos(int id) {
+int *Grafo::getVizinhos(int id)
+{
     return 0;
 }
 
-int Grafo::getGrau(int id) {
-  return 0;
+int Grafo::getGrau(int id)
+{
+    return 0;
 }
 
-int Grafo::getNumArestas() {
-  return 0;
+int Grafo::getNumArestas()
+{
+    return 0;
 }
 
-int Grafo::get_grau() {
-  int maiorGrau = 0;
-  
-  for(int i = 1; i <= ordem; i++) {
-      if(getGrau(i) > maiorGrau) {
-          maiorGrau = getGrau(i);
-      }
-  }
-  
-  return maiorGrau;
+int Grafo::get_grau()
+{
+    int maiorGrau = 0;
+
+    for (int i = 1; i <= ordem; i++)
+    {
+        if (getGrau(i) > maiorGrau)
+        {
+            maiorGrau = getGrau(i);
+        }
+    }
+
+    return maiorGrau;
 }
 
-bool Grafo::eh_completo() {
-    for (int i = 1; i <= ordem; i++) {
-        if (getGrau(i) != ordem - 1) { // Verifica se cada nó tem grau igual (ordem - 1)
+bool Grafo::eh_completo()
+{
+    for (int i = 1; i <= ordem; i++)
+    {
+        if (getGrau(i) != ordem - 1)
+        { // Verifica se cada nó tem grau igual (ordem - 1)
             return false;
         }
     }
-    
+
     return true;
 }
 
-int Grafo::n_conexo() {
+int Grafo::n_conexo()
+{
     // Aloca um array para marcar os nós visitados
-    bool* visitado = new bool[ordem + 1]; // +1 porque os nós começam de 1
-    for (int i = 1; i <= ordem; i++) {
+    bool *visitado = new bool[ordem + 1]; // +1 porque os nós começam de 1
+    for (int i = 1; i <= ordem; i++)
+    {
         visitado[i] = false;
     }
 
     int contador = 0;
 
     // Percorre todos os nós do grafo
-    for (int i = 1; i <= ordem; i++) {
-        if (!visitado[i]) {
+    for (int i = 1; i <= ordem; i++)
+    {
+        if (!visitado[i])
+        {
             // Se o nó não foi visitado, inicia uma DFS a partir dele
             DFS(i, visitado);
             contador++; // Incrementa o contador de componentes conexos
@@ -113,18 +137,22 @@ int Grafo::n_conexo() {
     return contador;
 }
 
-void Grafo::DFS(int no, bool* visitado) {
+void Grafo::DFS(int no, bool *visitado)
+{
     // Marca o nó como visitado
     visitado[no] = true;
 
     // Obtém os vizinhos do nó
-    int* vizinhos = getVizinhos(no);
+    int *vizinhos = getVizinhos(no);
 
     // Se o nó tiver vizinhos, percorre cada um deles
-    if (vizinhos) {
-        for (int i = 0; i < getGrau(no); i++) {
+    if (vizinhos)
+    {
+        for (int i = 0; i < getGrau(no); i++)
+        {
             int vizinho = vizinhos[i];
-            if (!visitado[vizinho]) {
+            if (!visitado[vizinho])
+            {
                 // Se o vizinho não foi visitado, realiza uma DFS a partir dele
                 DFS(vizinho, visitado);
             }
@@ -135,29 +163,41 @@ void Grafo::DFS(int no, bool* visitado) {
     }
 }
 
-void Grafo::calculaMenorDistancia() {
+void Grafo::calculaMenorDistancia()
+{
     // Inicializa a matriz de distâncias com um valor alto (infinito)
-    const float INF = 1e9f;  // Representa infinito para comparação
-    float** dist = new float*[ordem];
+    const float INF = 1e9f; // Representa infinito para comparação
+    float **dist = new float *[ordem];
 
-    for (int i = 0; i < ordem; i++) {
+    for (int i = 0; i < ordem; i++)
+    {
         dist[i] = new float[ordem];
-        for (int j = 0; j < ordem; j++) {
-            if (i == j) {
-                dist[i][j] = 0.0f;  // A distância de um nó para ele mesmo é 0
-            } else if (existeAresta(i + 1, j + 1)) {
+        for (int j = 0; j < ordem; j++)
+        {
+            if (i == j)
+            {
+                dist[i][j] = 0.0f; // A distância de um nó para ele mesmo é 0
+            }
+            else if (existeAresta(i + 1, j + 1))
+            {
                 dist[i][j] = getPesoAresta(i + 1, j + 1); // Assumindo que retorna float
-            } else {
+            }
+            else
+            {
                 dist[i][j] = INF;
             }
         }
     }
 
     // Floyd-Warshall para encontrar menores distâncias
-    for (int k = 0; k < ordem; k++) {
-        for (int i = 0; i < ordem; i++) {
-            for (int j = 0; j < ordem; j++) {
-                if (dist[i][k] < INF && dist[k][j] < INF) {
+    for (int k = 0; k < ordem; k++)
+    {
+        for (int i = 0; i < ordem; i++)
+        {
+            for (int j = 0; j < ordem; j++)
+            {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                {
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
@@ -168,10 +208,13 @@ void Grafo::calculaMenorDistancia() {
     float cont = 0.0f;
     int inicio;
     int fim;
-    
-    for (int i = 0; i < ordem; i++) {
-        for (int j = 0; j < ordem; j++) {
-            if (dist[i][j] < INF && dist[i][j] > cont) {
+
+    for (int i = 0; i < ordem; i++)
+    {
+        for (int j = 0; j < ordem; j++)
+        {
+            if (dist[i][j] < INF && dist[i][j] > cont)
+            {
                 cont = dist[i][j];
                 inicio = i;
                 fim = j;
@@ -182,23 +225,27 @@ void Grafo::calculaMenorDistancia() {
     cout << "Maior menor distância: (" << inicio + 1 << ", " << fim + 1 << ") - " << cont << endl;
 
     // Libera a memória alocada
-    for (int i = 0; i < ordem; i++) {
+    for (int i = 0; i < ordem; i++)
+    {
         delete[] dist[i];
     }
     delete[] dist;
 }
 
-
-void Grafo::deleta_primeira_aresta(int id) {
-    int* vizinhos = getVizinhos(id);
+void Grafo::deleta_primeira_aresta(int id)
+{
+    int *vizinhos = getVizinhos(id);
     int grau = getGrau(id);
 
-    if (grau == 0) return;  // Não há arestas para deletar
+    if (grau == 0)
+        return; // Não há arestas para deletar
 
-    int menor = vizinhos[0];  // Inicializa com o primeiro vizinho
+    int menor = vizinhos[0]; // Inicializa com o primeiro vizinho
 
-    for (int i = 1; i < grau; i++) {  // Começa do segundo vizinho
-        if (vizinhos[i] < menor) {
+    for (int i = 1; i < grau; i++)
+    { // Começa do segundo vizinho
+        if (vizinhos[i] < menor)
+        {
             menor = vizinhos[i];
         }
     }
@@ -207,60 +254,74 @@ void Grafo::deleta_primeira_aresta(int id) {
     deleta_aresta(id, menor);
 }
 
-void Grafo::carrega_grafo(const string& arquivo) {
+void Grafo::carrega_grafo(const string &arquivo)
+{
     ifstream file(arquivo.c_str());
-    
-    if (!file) {
+
+    if (!file)
+    {
         cerr << "Erro ao abrir o arquivo: " << arquivo << endl;
         exit(EXIT_FAILURE);
     }
-    
+
     string linha;
     int ordem_int;
 
     // Lê a descrição do grafo
-    if (getline(file, linha)) {
+    if (getline(file, linha))
+    {
         int direcionado_int, verticePonderado_int, arestaPonderada_int;
         sscanf(linha.c_str(), "%d %d %d %d", &ordem_int, &direcionado_int, &verticePonderado_int, &arestaPonderada_int);
         direcionado = direcionado_int;
         verticePonderado = verticePonderado_int;
         arestaPonderada = arestaPonderada_int;
     }
-    
+
     // Lê os pesos dos nós se ponderado
-    if (verticePonderado && getline(file, linha)) {
+    if (verticePonderado && getline(file, linha))
+    {
         float peso;
-        for (int id = 1; id <= ordem_int; ++id) {
+        for (int id = 1; id <= ordem_int; ++id)
+        {
             size_t pos = linha.find(" ");
-            if (pos != string::npos) {
+            if (pos != string::npos)
+            {
                 peso = atoi(linha.substr(0, pos).c_str());
                 linha = linha.substr(pos + 1);
-            } else {
+            }
+            else
+            {
                 peso = atoi(linha.c_str());
             }
             novo_no(id, peso);
         }
     }
-    
+
     // Lê as arestas
-    while (getline(file, linha)) {
-        if (linha.empty()) continue;
-        
+    while (getline(file, linha))
+    {
+        if (linha.empty())
+            continue;
+
         int origem, destino;
         float peso = 0.0f;
 
-        if (arestaPonderada) {
+        if (arestaPonderada)
+        {
             sscanf(linha.c_str(), "%d %d %f", &origem, &destino, &peso);
-        } else {
+        }
+        else
+        {
             sscanf(linha.c_str(), "%d %d", &origem, &destino);
         }
         nova_aresta(origem, destino, peso);
     }
-    
+
     file.close();
 }
 
-void Grafo::imprimeGrafo() {
+void Grafo::imprimeGrafo()
+{
     cout << "INFORMAÇÕES DO GRAFO: " << nomeArquivo << endl;
     cout << "Grau: " << get_grau() << endl;
     cout << "Ordem: " << ordem << endl;
@@ -269,41 +330,93 @@ void Grafo::imprimeGrafo() {
     cout << "Vertices Ponderados: " << (verticePonderado ? "Sim" : "Não") << endl;
     cout << "Arestas Ponderadas: " << (arestaPonderada ? "Sim" : "Não") << endl;
     cout << "Completo: " << (eh_completo() ? "Sim" : "Não") << endl;
-    
-    calculaMenorDistancia();
+
+    //calculaMenorDistancia();
     imprimeLista();
     imprimeMatriz();
 
-    int terminais[] = {1, 3, 2};  // Nó terminais do problema
+    int terminais[] = {1, 3, 4, 5}; // Nó terminais do problema
     int tamanho = sizeof(terminais) / sizeof(terminais[0]);
     steinerTree(terminais, tamanho);
 }
 
-void Grafo::imprimeLista() {
-  cerr << "Método imprimeLista chamado na classe base" << endl;
+void Grafo::imprimeLista()
+{
+    cerr << "Método imprimeLista chamado na classe base" << endl;
 }
 
-void Grafo::imprimeMatriz() {
-  cerr << "Método imprimeMatriz chamado na classe base" << endl;
+void Grafo::imprimeMatriz()
+{
+    cerr << "Método imprimeMatriz chamado na classe base" << endl;
 }
-
 
 // Implementação do método de Steiner
-void Grafo::steinerTree(int* terminais, int tamanho) {
+const int MAX_NODES = 6000; // Defina um tamanho máximo para os nós
+
+struct Pair {
+    float first;
+    int second;
+};
+
+void swap(Pair &a, Pair &b) {
+    Pair temp = a;
+    a = b;
+    b = temp;
+}
+
+void heapify(Pair arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left].first > arr[largest].first)
+        largest = left;
+
+    if (right < n && arr[right].first > arr[largest].first)
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void push(Pair arr[], int &n, Pair value) {
+    arr[n++] = value;
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+}
+
+Pair pop(Pair arr[], int &n) {
+    Pair root = arr[0];
+    arr[0] = arr[--n];
+    heapify(arr, n, 0);
+    return root;
+}
+
+void Grafo::steinerTree(int *terminais, int tamanho) {
     if (tamanho == 0) {
         cerr << "Nenhum nó terminal foi fornecido." << endl;
         return;
     }
 
-    set<int> conjuntoSteiner;
+    int conjuntoSteiner[MAX_NODES] = {0};
+    int conjuntoSteinerSize = 0;
+
     for (int i = 0; i < tamanho; i++) {
-        conjuntoSteiner.insert(terminais[i]);
+        conjuntoSteiner[conjuntoSteinerSize++] = terminais[i];
     }
 
-    vector<int> predecessor(ordem + 1, -1);
-    vector<float> distancia(ordem + 1, numeric_limits<float>::max());
+    int predecessor[MAX_NODES];
+    float distancia[MAX_NODES];
 
-    priority_queue<pair<float, int>, vector<pair<float, int>>, greater<pair<float, int>>> pq;
+    for (int i = 0; i < MAX_NODES; i++) {
+        predecessor[i] = -1;
+        distancia[i] = numeric_limits<float>::max();
+    }
+
+    Pair pq[MAX_NODES];
+    int pqSize = 0;
 
     // Inicializa os nós terminais
     for (int i = 0; i < tamanho; i++) {
@@ -313,79 +426,112 @@ void Grafo::steinerTree(int* terminais, int tamanho) {
             return;
         }
         distancia[t] = 0;
-        pq.push({0, t});
-    
+        push(pq, pqSize, {0, t});
 
-    // Algoritmo de Dijkstra modificado
-    while (!pq.empty()) {
-        int u = pq.top().second;
-        float dist_u = pq.top().first;
-        pq.pop();
+        // Algoritmo de Dijkstra modificado
+        while (pqSize > 0) {
+            Pair top = pop(pq, pqSize);
+            int u = top.second;
+            float dist_u = top.first;
 
-        if (dist_u > distancia[u]) continue;
+            if (dist_u > distancia[u])
+                continue;
 
-        int* vizinhos = getVizinhos(u);
-        int grau = getGrau(u);
+            int *vizinhos = getVizinhos(u);
+            int grau = getGrau(u);
 
-        if (vizinhos == nullptr) continue;  
+            if (vizinhos == nullptr)
+                continue;
 
-        for (int i = 0; i < grau; i++) {
-            int v = vizinhos[i];
-            if (v < 1 || v > ordem) continue;
+            for (int i = 0; i < grau; i++) {
+                int v = vizinhos[i];
+                if (v < 1 || v > ordem)
+                    continue;
 
-            float peso = getPesoAresta(u, v);
-            if (distancia[v] > dist_u + peso) {
-                distancia[v] = dist_u + peso;
-                predecessor[v] = u;
-                pq.push({distancia[v], v});
+                float peso = getPesoAresta(u, v);
+                if (distancia[v] > dist_u + peso) {
+                    distancia[v] = dist_u + peso;
+                    predecessor[v] = u;
+                    push(pq, pqSize, {distancia[v], v});
+                }
+            }
+
+            delete[] vizinhos;
+        }
+
+        // **Passo 2: Conectar os terminais na árvore de Steiner**
+        int arestasSteiner[MAX_NODES][2];
+        int arestasSteinerSize = 0;
+
+        for (int i = 0; i < tamanho; i++) {
+            int atual = terminais[i];
+            while (predecessor[atual] != -1) {
+                int pai = predecessor[atual];
+
+                // **Adiciona as arestas corretamente**
+                bool existe = false;
+                for (int j = 0; j < arestasSteinerSize; j++) {
+                    if ((arestasSteiner[j][0] == min(atual, pai) && (arestasSteiner[j][1] == max(atual, pai)))) {
+                        existe = true;
+                        break;
+                    }
+                }
+                if (!existe) {
+                    arestasSteiner[arestasSteinerSize][0] = min(atual, pai);
+                    arestasSteiner[arestasSteinerSize][1] = max(atual, pai);
+                    arestasSteinerSize++;
+                }
+
+                bool noExiste = false;
+                for (int j = 0; j < conjuntoSteinerSize; j++) {
+                    if (conjuntoSteiner[j] == atual) {
+                        noExiste = true;
+                        break;
+                    }
+                }
+                if (!noExiste) {
+                    conjuntoSteiner[conjuntoSteinerSize++] = atual;
+                }
+
+                noExiste = false;
+                for (int j = 0; j < conjuntoSteinerSize; j++) {
+                    if (conjuntoSteiner[j] == pai) {
+                        noExiste = true;
+                        break;
+                    }
+                }
+                if (!noExiste) {
+                    conjuntoSteiner[conjuntoSteinerSize++] = pai;
+                }
+
+                atual = pai;
             }
         }
 
-        delete[] vizinhos;  
-    }
-
-    // **Passo 2: Conectar os terminais na árvore de Steiner**
-    set<pair<int, int>> arestasSteiner;
-    for (int i = 0; i < tamanho; i++) {
-        int atual = terminais[i];
-        while (predecessor[atual] != -1) {
-            int pai = predecessor[atual];
-
-            // **Adiciona as arestas corretamente**
-            if (arestasSteiner.find({min(atual, pai), max(atual, pai)}) == arestasSteiner.end()) {
-                arestasSteiner.insert({min(atual, pai), max(atual, pai)});
+        // **Depuração extra**
+        cout << "Predecessores:" << endl;
+        for (int i = 1; i <= ordem; i++) {
+            if (predecessor[i] != -1) {
+                cout << "Nó " << i << " -> Predecessor: " << predecessor[i] << endl;
             }
-
-            conjuntoSteiner.insert(atual);
-            conjuntoSteiner.insert(pai);
-
-            atual = pai;
         }
-    }
 
-    // **Depuração extra**
-    cout << "Predecessores:" << endl;
-    for (int i = 1; i <= ordem; i++) {
-        if (predecessor[i] != -1) {
-            cout << "Nó " << i << " -> Predecessor: " << predecessor[i] << endl;
-        }
-    }
-
-    // **Imprime os nós da Árvore de Steiner**
-    cout << "Árvore de Steiner encontrada com os nós: ";
-    for (int no : conjuntoSteiner) {
-        cout << no << " ";
-    }
-    cout << endl;
-
-    // **Imprime as arestas corretamente**
-    cout << "E com as arestas: ";
-    if (arestasSteiner.empty()) {
-        cout << "Nenhuma aresta encontrada." << endl;
-    } else {
-        for (auto aresta : arestasSteiner) {
-            cout << "(" << aresta.first << ", " << aresta.second << ") ";
+        // **Imprime os nós da Árvore de Steiner**
+        cout << "Árvore de Steiner encontrada com os nós: ";
+        for (int i = 0; i < conjuntoSteinerSize; i++) {
+            cout << conjuntoSteiner[i] << " ";
         }
         cout << endl;
+
+        // **Imprime as arestas corretamente**
+        cout << "E com as arestas: ";
+        if (arestasSteinerSize == 0) {
+            cout << "Nenhuma aresta encontrada." << endl;
+        } else {
+            for (int i = 0; i < arestasSteinerSize; i++) {
+                cout << "(" << arestasSteiner[i][0] << ", " << arestasSteiner[i][1] << ") ";
+            }
+            cout << endl;
+        }
     }
 }
