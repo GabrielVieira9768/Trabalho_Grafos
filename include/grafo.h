4ae8037253a8c92eb20feaca0eba9695196d6a12
Vitor Fernandes Reis <vitor.fernandes@estudante.ufjf.br>
2025-03-16 23:40:34 +0000

    Adição de Comentários

--- include/grafo.h
+++ include/grafo.h
@@ -16,38 +16,37 @@ class Grafo {
     public:
         virtual ~Grafo() {}
         
-        void imprimeGrafo();
-        int get_ordem();
-        bool eh_direcionado();
-        bool vertice_ponderado();
-        bool aresta_ponderada();
+        void imprimeGrafo();            //Autores: Carlos, Gabriel, Maria, Vítor. Imprime informações do grafo
+        int get_ordem();                //Autor: Gabriel. Retorna a ordem do grafo
+        bool eh_direcionado();          //Autor: Gabriel. Retorna se o grafo é direcionado
+        bool vertice_ponderado();       //Autor: Gabriel. Retorna se o grafo tem vértices ponderados
+        bool aresta_ponderada();        //Autor: Gabriel. Retorna se o grafo tem arestas ponderadas
         
-        int get_grau();
-        bool eh_completo();
-        int n_conexo();
+        int get_grau();                 //Autores: Carlos, Gabriel, Maria. Retorna o grau do grafo
+        bool eh_completo();             //Autores: Carlos, Gabriel. Verifica se o grafo é completo
+        int n_conexo();                 //Autores: Carlos, Gabriel, Maria. Retorna o número de componentes conexas
         
-        void carrega_grafo(const string& arquivo);
-        void DFS(int no, bool* visitado);
-        void calculaMenorDistancia();
-        void deleta_primeira_aresta(int id);
-        void steinerTree(int *terminais, int tamanho, bool randomizado = false, float alpha = 0.3 , bool reativo = false);
-        int* geraTerminaisAleatorios(int ordem, int& tamanhoTerminais);
-        //void steinerTreeGRASP(int *terminais, int tamanho, int numIteracoes, float alpha);
+        void carrega_grafo(const string& arquivo);      //Autora: Maria. Carrega o grafo a partir de um arquivo
+        void DFS(int no, bool* visitado);               //Autores: Carlos, Gabriel, Maria.. Algoritmo de busca em profundidade   
+        void calculaMenorDistancia();                   //Autora: Maria. Calcula a menor distância entre dois vértices    
+        void deleta_primeira_aresta(int id);            //Autora: Maria. Deleta a primeira aresta de um nó
+        void steinerTree(int *terminais, int tamanho, bool randomizado = false, float alpha = 0.3 , bool reativo = false); //Autores: Carlos, Gabriel, Maria, Vítor. Algoritmo de Steiner Tree
+        int* geraTerminaisAleatorios(int ordem, int& tamanhoTerminais); //Autor: Vítor. Gera terminais aleatórios
         
-        virtual void novo_no(int id, float peso);
-        virtual void deleta_no(int no);
-        virtual void nova_aresta(int origem, int destino, float peso);
-        virtual void deleta_aresta(int origem, int destino);
+        virtual void novo_no(int id, float peso);                       //Autores: Carlos, Gabriel. Adiciona um novo nó ao grafo
+        virtual void deleta_no(int no);                                 //Autores: Carlos, Gabriel. Deleta um nó do grafo
+        virtual void nova_aresta(int origem, int destino, float peso);  //Autores: Carlos, Gabriel. Adiciona uma nova aresta ao grafo
+        virtual void deleta_aresta(int origem, int destino);            //Autores: Carlos, Gabriel. Deleta uma aresta do grafo
         
-        virtual bool existeNo(int id); // Verifica se existe um nó específico
-        virtual bool existeAresta(int origem, int destino); // Verifica se existe uma aresta específica
-        virtual int getGrau(int id); // Retorna o Grau de um vértice
-        virtual int* getVizinhos(int id); // Retorna um array com todos os vizinhos de um nó
-        virtual int getNumArestas(); // Retorna o número de arestas do grafo
-        virtual float getPesoAresta(int origem, int destino); // Retorna o peso de uma aresta
+        virtual bool existeNo(int id);                          //Autor: Gabriel. Verifica se um nó existe
+        virtual bool existeAresta(int origem, int destino);     //Autor: Gabriel. Verifica se uma aresta existe
+        virtual int getGrau(int id);                            //Autores: Gabriel e Maria. Retorna o grau de um nó       
+        virtual int* getVizinhos(int id);                       //Autores: Gabriel, Carlos. Retorna os vizinhos de um nó
+        virtual int getNumArestas();                            //Autor: Gabriel. Retorna o número de arestas do grafo
+        virtual float getPesoAresta(int origem, int destino);   //Autor: Gabriel. Retorna o peso de uma aresta
         
-        virtual void imprimeLista(); // Imprime o grafo na forma de lista encadeada
-        virtual void imprimeMatriz(); // Imprime o grafo na forma de matriz
+        virtual void imprimeLista();                            //Autor: Gabriel. Imprime o grafo na forma de lista encadeada 
+        virtual void imprimeMatriz();                           //Autor: Gabriel. Imprime o grafo na forma de matriz de adjacência
 };
 
 #endif
